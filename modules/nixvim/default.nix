6ec66d1e8e76b3c38bdd1183f6f82ee9246fe3c3
Bosco Vallejo-Nágera <bosco@vallejonagera.xyz>
2025-05-12 13:53:50 +0200

    nixvim some keymaps

--- modules/nixvim/default.nix
+++ modules/nixvim/default.nix
@@ -14,12 +14,24 @@
     lsp.servers = {
       gopls.enable = true;
     };
+    keymaps = [
+      {
+        action = "<cmd>Oil<CR>";
+        key = "-";
+      }
+      {
+        action = "<cmd>Telescope find_files<CR>";
+        key = "<leader>sf";
+	options.desc = "Search files with telescope";
+      }
+    ];
     colorschemes.tokyonight.enable = true;
     plugins = {
       oil.enable = true;
       telescope.enable = true;
       web-devicons.enable = true;
       treesitter.enable = true;
+      which-key.enable = true;
       cmp = {
         enable = true;
         autoEnableSources = true;
