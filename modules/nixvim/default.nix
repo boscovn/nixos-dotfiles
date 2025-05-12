2a618fd307836b4c30fe5c4a3e145765c7ab45eb
Bosco Vallejo-Nágera <bosco@vallejonagera.xyz>
2025-05-12 21:19:07 +0200

    Nixvim changes

--- modules/nixvim/default.nix
+++ modules/nixvim/default.nix
@@ -2,6 +2,7 @@
   config,
   pkgs,
   inputs,
+  lib,
   ...
 }:
 {
@@ -11,8 +12,61 @@
   programs.nixvim = {
     enable = true;
     globals.mapleader = " ";
+    opts = {
+      relativenumber = true;
+    };
     lsp = {
-      enable = true;
+      keymaps = [
+        {
+          key = "gd";
+          lspBufAction = "definition";
+        }
+        {
+          key = "gD";
+          lspBufAction = "references";
+        }
+        {
+          key = "gt";
+          lspBufAction = "type_definition";
+        }
+        {
+          key = "gi";
+          lspBufAction = "implementation";
+        }
+        {
+          key = "K";
+          lspBufAction = "hover";
+        }
+        # {
+        #   action = lib.nixvim.mkRaw "function() vim.diagnostic.jump({ count=-1, float=true }) end";
+        #   key = "<leader>k";
+        # }
+        # {
+        #   action = lib.nixvim.mkRaw "function() vim.diagnostic.jump({ count=1, float=true }) end";
+        #   key = "<leader>j";
+        # }
+        {
+          action = "<CMD>LspStop<Enter>";
+          key = "<leader>lx";
+        }
+        {
+          action = "<CMD>LspStart<Enter>";
+          key = "<leader>ls";
+        }
+        {
+          action = "<CMD>LspRestart<Enter>";
+          key = "<leader>lr";
+        }
+        # {
+        #   action = lib.nixvim.mkRaw "require('telescope.builtin').lsp_definitions";
+        #   key = "gd";
+        # }
+        {
+          action = "<CMD>Lspsaga hover_doc<Enter>";
+          key = "K";
+        }
+      ];
+
       servers = {
         gopls.enable = true;
       };
@@ -30,6 +84,7 @@
     ];
     colorschemes.tokyonight.enable = true;
     plugins = {
+      lsp.enable = true;
       oil.enable = true;
       telescope.enable = true;
       web-devicons.enable = true;
