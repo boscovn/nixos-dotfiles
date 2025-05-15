2359725c9bc520fa8c1540e4511fd49b65a6ea28
Bosco Vallejo-Nágera <bosco@vallejonagera.xyz>
2025-05-15 20:45:46 +0200

    cmp nvim

--- modules/nixvim/default.nix
+++ modules/nixvim/default.nix
@@ -90,13 +90,30 @@
       web-devicons.enable = true;
       treesitter.enable = true;
       which-key.enable = true;
+      luasnip.enable = true;
+      cmp-omni.enable = true;
+      cmp-dap.enable = true;
+      cmp-nvim-lsp.enable = true;
+      cmp-nvim-lsp-document-symbol.enable = true;
+      cmp-nvim-lsp-signature-help.enable = true;
+      cmp-dictionary.enable = true;
       cmp = {
         enable = true;
         autoEnableSources = true;
+        settings.mapping = {
+          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-f>" = "cmp.mapping.scroll_docs(4)";
+          "<C-Space>" = "cmp.mapping.complete()";
+          "<C-e>" = "cmp.mapping.close()";
+          "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+          "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+        };
         settings.sources = [
           { name = "nvim_lsp"; }
           { name = "path"; }
           { name = "buffer"; }
+          { name = "luasnip"; }
         ];
       };
 
