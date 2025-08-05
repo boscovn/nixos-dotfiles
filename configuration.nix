796db515ae01c85a16bc7f898e1aa6697e2b1034
Bosco Vallejo-Nágera <bosco@vallejonagera.xyz>
2025-08-05 17:53:56 +0200

    Made changes

--- configuration.nix
+++ configuration.nix
@@ -96,14 +96,14 @@
   };
 
   # TODO: fix it, make the keyring work
-  services.greetd.enable = true;
-  services.greetd.settings = {
-    default_session = {
-      command = "${pkgs.cage}/bin/cage -s -- gtkgreet";
-      user = "greeter";
-    };
-  };
-  security.pam.services.greetd.enableGnomeKeyring = true;
+  # services.greetd.enable = true;
+  # services.greetd.settings = {
+  #   default_session = {
+  #     command = "${pkgs.cage}/bin/cage -s -- gtkgreet";
+  #     user = "greeter";
+  #   };
+  # };
+  # security.pam.services.greetd.enableGnomeKeyring = true;
   services.gnome.gnome-keyring.enable = true;
 
   # Allow unfree packages
